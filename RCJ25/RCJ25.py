054aa204ab6aecbce1423980ebd2a32ae903596d
Ozer Tomer <ozertomer@gmail.com>
2025-01-09 16:50:48 +0200

    09-01-2025V2

--- RCJ25/RCJ25.py
+++ RCJ25/RCJ25.py
@@ -1,4 +1,4 @@
-from machine import Pin, SoftI2C, I2C
+from machine import Pin, SoftI2C, I2C, UART
 from time import sleep_ms
 from math import atan2, sqrt
 import time
@@ -6,15 +6,12 @@ from L298N_MINI import motor
 from MPU6050 import MPU6050
 
 
-
-
-
-class RCJ25: 
+class RCJ25:
     def __init__(self):
         self.gyroDefined = 0
         self.previous_error = 0
         self.integral = 0
-        self.last_time = time.time() 
+        self.last_time = time.time()
         self.i2c_scl = 22
         self.i2c_sda = 21
         self.i2c = I2C(0, scl=Pin(self.i2c_scl), sda=Pin(self.i2c_sda))
@@ -22,14 +19,17 @@ class RCJ25:
         self.motor2 = motor(25, 26)
         self.motor3 = motor(27, 14)
         self.motor4 = motor(12, 13)
-        self.gyro = MPU6050()
+        #self.gyro = MPU6050()
+        self.uart = UART(2, tx=17, rx=16, baudrate=115200)
 
-
+        self.uart.init(115200, bits=8, parity=None, stop=1)
+        print("ESP32 UART initialized.")
+        self.i = 0
 
     def pid_calc(self, current_value, Kp, Ki, Kd):
         current_time = time.time()
         dt = current_time - self.last_time
-        self.last_time = current_time  
+        self.last_time = current_time
 
         error = self.setpoint - current_value
 
@@ -37,27 +37,18 @@ class RCJ25:
 
         derivative = (error - self.previous_error) / dt if dt > 0 else 0
 
-        output = Kp * error + Ki * integral + Kd * derivative
+        output = Kp * error + Ki * self.integral + Kd * derivative
 
         self.previous_error = error
 
         return output
 
-#    def DefineGyro(self):
-#        if self.gyroDefined == 0:
-#            self.gyro = MPU6050()
-#            self.gyroDefined = 1
-        
-
-        
     def read_gyro(self):
         data = self.gyro.read_gyro_data()
         return data
 
-
     def get_yaw(self):
         return int(self.gyro.getYaw())
-
 
     def get_pitch(self):
         return self.gyro.getPitch()
@@ -67,21 +58,34 @@ class RCJ25:
 
     def turnRight(self, speed, setpoint):
         print("Right")
-        
+
     def turnLeft(self, speed, setpoint):
         print("Left")
-        
+
     def turn(self, speed, setpoint):
         self.turnRight(speed, setpoint) if setpoint > 0 else self.turnLeft(speed, setpoint)
 
+    def receive_line_data(self):
+        if self.uart.any():  # Check if data is available
+            try:
+                line = self.uart.readline()  # Read the line from UART
+                if line:
+                    data = line.decode('utf-8').strip()
+                    print(f"Received data: {data}")
+                else:
+                    print("No data received!")
+            except Exception as e:
+                print(f"Error receiving data: {e}")
+        else:
+            self.i += 1
+            print(f"Waiting for data...{self.i}")
+
 
     def run(self):
-        print(self.get_yaw())
-        
+        self.receive_line_data()
 
 
+robot = RCJ25()
+while True:
+    robot.run()
 
-
-
-
-print("Hello World!")
